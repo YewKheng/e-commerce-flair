7ce8077583ae47897543843c4ac5610c5ec1da75
Yew Kheng Kok <144677450+YewKheng@users.noreply.github.com>
2024-01-19 02:43:12 +0000

    first commit

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Flair",
+  description: "Expertly desgined products for the workspace, home or travel.",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+}
